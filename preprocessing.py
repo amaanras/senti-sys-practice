2c75f062310104924e41c05129ba81d8be9aa6fc
amaan ras <amaanras.petersen@gmail.com>
2023-07-12 20:26:47 +0200

    stemming and stopword removal

--- preprocessing.py
+++ preprocessing.py
@@ -16,3 +16,14 @@ df['Review Text'] = df['Review Text'].str.replace('[^\w\s]','') # removing the p
 df['Review Text'] = df['Review Text'].str.lower() # converting all letter to lower case to make is easier and cosistant
 df['Review Text'] = df['Review Text'].str.split() # tokenization
 
+# stopword removal....common words that dont carry any signaficant meaning
+stop_words = set(stopwords.words('english'))
+df['Review Text'] = df['Review Text'].apply(lambda x: [word for word in x if word not in stop_words])
+
+# stemming avoiding having multiple versions of the same word
+stemmer = PorterStemmer()
+df['Review Text'] = df['Review Text'].apply(lambda x: [stemmer.stem(word) for word in x])
+
+# displaying the preprocessed dataframe
+#print(df.to_string(index=False)) 
+print(df.head())
